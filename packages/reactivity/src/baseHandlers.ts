0e3e47ea371bdead451a0edf87e324a2752a98fa
xing-6464 <xing_6464@foxmail.com>
2023-05-14 23:05:04 +0800

    createGetter和createSetter

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -1,1 +1,36 @@
-export const mutableHandlers: ProxyHandler<object> = {}
+import { track, trigger } from './effect'
+
+const get = createGetter()
+const set = createSetter()
+
+function createGetter() {
+  return function get(target: object, key: string | symbol, receiver: object) {
+    const res = Reflect.get(target, key, receiver)
+
+    // 依赖收集
+    track(target, key)
+
+    return res
+  }
+}
+
+function createSetter() {
+  return function set(
+    target: object,
+    key: string | symbol,
+    value: unknown,
+    receiver: object
+  ) {
+    const result = Reflect.set(target, key, value, receiver)
+
+    // 触发依赖
+    trigger(target, key, value)
+
+    return result
+  }
+}
+
+export const mutableHandlers: ProxyHandler<object> = {
+  get,
+  set
+}
