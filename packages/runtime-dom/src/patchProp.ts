6cc152d938ddca507618cc37e997cf5725fd7561
xing-6464 <17621404964@163.com>
2024-05-29 18:36:17 +0800

    element下的事件挂载和更新

--- packages/runtime-dom/src/patchProp.ts
+++ packages/runtime-dom/src/patchProp.ts
@@ -3,6 +3,7 @@ import { patchClass } from './modules/class'
 import { patchDOMProp } from './modules/props'
 import { patchAttr } from './modules/attrs'
 import { patchStyle } from './modules/style'
+import { patchEvent } from './modules/event'
 
 export const patchProp = (
   el: Element,
@@ -15,6 +16,7 @@ export const patchProp = (
   } else if (key === 'style') {
     patchStyle(el, prevValue, nextValue)
   } else if (isOn(key)) {
+    patchEvent(el, key, prevValue, nextValue)
   } else if (shouldSetAsProp(el, key)) {
     patchDOMProp(el, key, nextValue)
   } else {
