150b2f5acbab091df097f67915faafbffb067f2a
xing-6464 <17621404964@163.com>
2024-07-12 04:11:14 +0800

    实现compositionApi和setup函数

--- packages/runtime-core/src/component.ts
+++ packages/runtime-core/src/component.ts
@@ -1,5 +1,5 @@
 import { reactive } from '@vue/reactivity'
-import { isObject } from '@vue/shared'
+import { isFunction, isObject } from '@vue/shared'
 import { onBeforeMount, onMounted } from './apiLifecycle'
 
 let uid = 0
@@ -37,13 +37,31 @@ export function setupComponent(instance) {
 }
 
 function setupStatefulComponent(instance) {
+  // 获取组件实例
+  const Component = instance.type
+
+  const { setup } = Component
+  if (setup) {
+    const setupResult = setup()
+    handleSetupResult(instance, setupResult)
+  } else {
+    finishComponentSetup(instance)
+  }
+}
+
+export function handleSetupResult(instance, setupResult) {
+  if (isFunction(setupResult)) {
+    instance.render = setupResult
+  }
   finishComponentSetup(instance)
 }
 
 export function finishComponentSetup(instance) {
   const Component = instance.type
 
-  instance.render = Component.render
+  if (!instance.render) {
+    instance.render = Component.render
+  }
 
   applyOptions(instance)
 }
